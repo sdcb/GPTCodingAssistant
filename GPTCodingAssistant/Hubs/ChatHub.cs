42f759bd4f1c17d898c59f59c278e5d97a20b7ad
sdcb <sdflysha@qq.com>
2023-04-21 11:38:22 +0800

    Add backend chathub.

---
+++ GPTCodingAssistant/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using OpenAI_API.Chat;
+
+namespace GPTCodingAssistant.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async IAsyncEnumerable<char> Chat(ChatMessage[] chatMessages)
+        {
+            string response = $"Hello World: {chatMessages.Length}";
+            foreach (char c in response)
+            {
+                yield return c;
+                await Task.Delay(10);
+            }
+        }
+    }
+}
